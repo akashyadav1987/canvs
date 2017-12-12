7742b5141dab7f77b949884d6fe6e3935c5b39c0
ashish <ashish@getzyme.com>
2017-12-12 23:34:57 +0530

    about us added

--- app/src/main/java/com/canvas/fragment/AboutUsFragment.java
+++ app/src/main/java/com/canvas/fragment/AboutUsFragment.java
@@ -1,10 +1,13 @@
 package com.canvas.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.canvas.R;
 import com.canvas.common.CommonFragment;
@@ -14,11 +17,30 @@ import com.canvas.common.CommonFragment;
  */
 
 public class AboutUsFragment extends CommonFragment {
+    Button button_mean,button_crux;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.about_us,null);
         screenTitle="ABOUT US";
+        button_mean=view.findViewById(R.id.mean_button);
+        button_mean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLink("http://www.meangenius.com");
+            }
+        });
+        button_crux=view.findViewById(R.id.crux_button);
+        button_crux.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              openLink("http://www.meangenius.com");
+            }
+        });
         return view;
+    }
+    private void openLink(String link){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+        startActivity(browserIntent);
     }
 }
